21cfba3084313b2e3368f61263c0afddc269ec04
Husnain5312 <husnainbasheer34356@gmail.com>
2026-07-10 18:04:07 +0500

    removed title

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_3/auth/views/login.dart';
 import 'package:flutter_application_3/firebase_options.dart';
 
-void main()async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
-
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Firebase App",
-      debugShowCheckedModeBanner: false,
-      home: Login(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Login());
   }
 }
