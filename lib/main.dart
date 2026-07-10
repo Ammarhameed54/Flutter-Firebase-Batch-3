42341abb5031489dd5ae7c2e7a868155189ab9d8
ammarhameed54 <chaudharyammar@gmail.com>
2026-07-09 17:45:14 -0700

    title updated

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Firebase App",
       debugShowCheckedModeBanner: false,
       home: Login(),
     );
