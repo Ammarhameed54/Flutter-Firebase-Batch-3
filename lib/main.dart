47e17cb6a3c76f7b890d43239409351565c54955
ammarhameed54 <chaudharyammar@gmail.com>
2026-07-10 18:07:28 -0700

    added textscale factor and portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_application_3/auth/views/login.dart';
 import 'package:flutter_application_3/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Lock the app to portrait mode
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(MyApp());
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -17,7 +23,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "Firebase App",
       debugShowCheckedModeBanner: false,
-      home: Login(),
+      builder: (context, child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(
+            textScaler: const TextScaler.linear(1.0), // Flutter 3.16+
+          ),
+          child: child!,
+        );
+      },
+      home: const Login(),
     );
   }
 }
